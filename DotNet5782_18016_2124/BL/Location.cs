66ee7ba980b5eebee50966c2fc456a682eb32886
חגית אורנשטיין <chagito3440@gmail.com>
2021-12-05 01:23:09 +0200

    cobl

---
+++ DotNet5782_18016_2124/BL/Location.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBL.BO
+{
+    public class Location
+    {
+        public double Longitude { get; set; }
+        public double Lattitude { get; set; }
+
+
+        public override String ToString()
+        {
+            String result = "";
+
+            result += $"Longitude is {Longitude},\n";
+            result += $"Lattitude is {Lattitude},\n";
+            return result;
+        }
+    }
+
+}
+
+
