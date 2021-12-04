03da015d3ce0bc47746d9830d6a4f15eef284ea9
avital elchadad <avitalelchadad2002@gmail.com>
2021-12-04 23:33:03 +0200

    hhh

---
+++ DotNet5782_18016_2124/Targil0/Program1687.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Targil0
+{
+
+    partial class Program
+    {
+        static void Welcome1687()
+        {
+            Console.WriteLine("I am also here! ");
+        }
+    }
+}
