12330bcaf90e192a8272da0650bc0e5d2357f813
חגית אורנשטיין <chagito3440@gmail.com>
2021-12-04 23:32:11 +0200

    intial code+welcome program

---
+++ DotNet5782_18016_2124/Targil0/Program8016.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8016();
+            Welcome1867();
+            Console.ReadKey();
+        }
+        static partial void Welcome1867();
+
+        static void Welcome8016()
+        {
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine($"{username}, welcome to my first console application");
+        }
+    }
+}
