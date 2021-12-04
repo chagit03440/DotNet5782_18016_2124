e50e3ac23e46022411e5a1cd2c31d289dbbbee1b
חגית אורנשטיין <chagito3440@gmail.com>
2021-12-05 00:48:44 +0200

    dal

---
+++ DotNet5782_18016_2124/DAL/DroneCharge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct DroneCharge
+        {
+            public int DroneId { get; set; }
+            public int StationId { get; set; }
+            public override String ToString()
+            {
+                String result = "";
+                result += $"DroneId is {DroneId},\n";
+                result += $"StationId is {StationId},\n";
+                return result;
+            }
+        }
+    }
+
+}
---
+++ DotNet5782_18016_2124/DAL/enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public enum WeightCategories { low, middle, heavy };
+        public enum DroneStatuses { free, maintenance, shipping };
+        public enum Priorities { regular, fast, emergency };
+
+    }
+
+}
